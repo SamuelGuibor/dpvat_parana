50c18364f8f77f449dde8f18e01c8193817d8a78
Otto <ottoengelhardt18@gmail.com>
2025-03-24 19:29:52 -0300

    feat: add objetivos section

---
+++ app/section/objetivos.tsx
@@ -0,0 +1,23 @@
+const Objetivos = () => {
+  return (
+    <section className="flex flex-col items-center text-center py-10 px-4">
+      <div className="max-w-4xl">
+        <h1 className="text-4xl md:text-5xl font-bold tracking-tight">
+          Nosso Objetivo
+        </h1>
+        <p className="text-base md:text-lg leading-relaxed pt-6 max-w-3xl mx-auto">
+          Nos dedicamos a proteger os direitos e interesses dos nossos clientes
+          com acordos extrajudiciais, garantindo uma justa e rápida indenização.
+          Nosso compromisso é com a transparência, a ética e a eficácia,
+          desburocratizando procedimentos administrativos de seguradoras,
+          acompanhando o cliente desde a obtenção de documentos até a realização
+          de perícia para garantir paz de espírito e resultados positivos.
+          Buscamos inovação contínua e construção de relacionamentos duradouros,
+          visando a excelência e o empoderamento dos nossos clientes.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Objetivos;
