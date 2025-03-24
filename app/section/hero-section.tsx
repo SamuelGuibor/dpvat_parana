c14eb1540189f7eb99ea671a391682d3016ba255
Otto <ottoengelhardt18@gmail.com>
2025-03-24 19:29:22 -0300

    chore: change hero

--- app/section/hero-section.tsx
+++ app/section/hero-section.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
-import Link from "next/link"
-import { Button } from "@/app/_components/ui/button"
-import { HeroHeader } from "./hero9-header"
-import { ChevronRight } from "lucide-react"
+import Link from "next/link";
+import { Button } from "@/app/_components/ui/button";
+import { HeroHeader } from "./hero9-header";
+import { ChevronRight } from "lucide-react";
 
 export default function HeroSection() {
   return (
@@ -10,18 +10,22 @@ export default function HeroSection() {
       <HeroHeader />
       <main className="overflow-x-hidden">
         <section>
-          <div className="py-24 md:pb-32 lg:pb-36 lg:pt-72">
+          <div className="py-32 md:pb-40 lg:pb-48 lg:pt-80">
             <div className="relative mx-auto flex max-w-7xl flex-col px-6 lg:block lg:px-12">
               <div className="mx-auto max-w-lg text-center lg:ml-0 lg:max-w-full lg:text-left">
                 <h1 className="mt-8 max-w-2xl text-balance text-5xl md:text-6xl lg:mt-16 xl:text-7xl">
-                    DPVAT Paraná
+                  DPVAT Paraná
                 </h1>
                 <p className="mt-8 max-w-2xl text-balance text-lg">
-                Receba uma indenização pelas lesões do seu acidente
+                  Receba uma indenização pelas lesões do seu acidente
                 </p>
 
                 <div className="mt-12 flex flex-col items-center justify-center gap-2 sm:flex-row lg:justify-start">
-                  <Button asChild size="lg" className="h-12 rounded-full pl-5 pr-3 text-base">
+                  <Button
+                    asChild
+                    size="lg"
+                    className="h-12 rounded-full pl-5 pr-3 text-base"
+                  >
                     <Link href="#link">
                       <span className="text-nowrap">Fale Conosco</span>
                       <ChevronRight className="ml-1" />
@@ -42,6 +46,5 @@ export default function HeroSection() {
         </section>
       </main>
     </>
-  )
+  );
 }
-
