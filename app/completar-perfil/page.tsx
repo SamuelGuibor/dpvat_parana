fcf811f4105039a6401bd2646fca84fa5799f22f
Otto <ottoengelhardt18@gmail.com>
2025-03-25 11:17:22 -0300

    feat: ensure form is validated and save on db

--- app/completar-perfil/page.tsx
+++ app/completar-perfil/page.tsx
@@ -1,227 +1,247 @@
 "use client";
 
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { updateUserProfile } from "../_actions/userActions";
-import { Button } from "@/app/_components/ui/button";
-import { Input } from "@/app/_components/ui/input";
-import { Label } from "@/app/_components/ui/label";
-import Image from "next/image";
-import { Calendar } from "@/app/_components/ui/calendar";
+import { Button } from "../_components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../_components/ui/form";
+import { Input } from "../_components/ui/input";
 import {
   Popover,
   PopoverTrigger,
   PopoverContent,
-} from "@/app/_components/ui/popover";
-import { format } from "date-fns";
+} from "../_components/ui/popover";
+import { Calendar } from "../_components/ui/calendar";
 import { z } from "zod";
 import { isValidCpf } from "./helpers/cpf";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { format } from "date-fns";
+import Image from "next/image";
+import { Loader2Icon } from "lucide-react";
+import { useState } from "react";
 
 const formSchema = z.object({
-  name: z.string().trim().min(1, {
-    message: "O nome é obrigatório!",
-  }),
   cpf: z
     .string()
     .trim()
-    .min(1, {
-      message: "O CPF é obrigatório!",
-    })
-    .refine((value) => isValidCpf(value), {
-      message: "CPF invalido",
+    .min(1, { message: "O CPF é obrigatório!" })
+    .refine((value) => isValidCpf(value), { message: "CPF inválido!" }),
+  data_nasc: z.string().refine((value) => !isNaN(new Date(value).getTime()), {
+    message: "Data de nascimento inválida!",
+  }),
+  rua: z
+    .string()
+    .trim()
+    .min(1, { message: "A rua é obrigatória!" })
+    .startsWith("Rua", { message: "A rua deve começar com 'Rua'" }),
+  bairro: z.string().trim().min(1, { message: "O bairro é obrigatório!" }),
+  numero: z
+    .string()
+    .trim()
+    .regex(/^\d+$/, { message: "O número deve conter apenas dígitos!" })
+    .min(1, { message: "O número é obrigatório!" }),
+  cep: z
+    .string()
+    .trim()
+    .regex(/^\d{5}-\d{3}$/, {
+      message: "O CEP deve estar no formato 00000-000!",
     }),
 });
 
-type formSchema = z.infer<typeof formSchema>;
+type FormSchema = z.infer<typeof formSchema>;
 
 export default function CompletarPerfil() {
   const { data: session } = useSession();
   const router = useRouter();
 
-  const [formData, setFormData] = useState({
-    cpf: "",
-    data_nasc: "",
-    rua: "Rua",
-    bairro: "",
-    numero: "",
-    cep: "",
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      cpf: "",
+      data_nasc: "",
+      rua: "Rua",
+      bairro: "",
+      numero: "",
+      cep: "",
+    },
   });
 
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  if (!session) return;
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-
-    if (name === "cpf") {
-      let formattedCpf = value.replace(/\D/g, "");
-      formattedCpf = formattedCpf.replace(
-        /(\d{3})(\d{3})(\d{3})(\d{2})/,
-        "$1.$2.$3-$4"
-      );
-      setFormData({ ...formData, cpf: formattedCpf });
-    } else if (name === "cep") {
-      let formattedCep = value.replace(/\D/g, "");
-      formattedCep = formattedCep.replace(/(\d{5})(\d{3})/, "$1-$2");
-      setFormData({ ...formData, cep: formattedCep });
-    } else if (name === "numero") {
-      const formattedNumero = value.replace(/\D/g, "");
-      setFormData({ ...formData, numero: formattedNumero });
-    } else if (name !== "rua") {
-      setFormData({ ...formData, [name]: value });
-    }
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const onSubmit = async (data: FormSchema) => {
     setLoading(true);
-    setError("");
-
-    const formattedDataNasc = new Date(formData.data_nasc).toISOString();
-
-    const updatedData = {
-      ...formData,
-      data_nasc: formattedDataNasc,
-    };
-
     try {
-      await updateUserProfile(updatedData);
+      const formattedData = {
+        ...data,
+        data_nasc: new Date(data.data_nasc).toISOString(),
+      };
+      console.log("Dados formatados para salvar:", formattedData);
+      await updateUserProfile(formattedData);
       router.push("/");
-    } catch (err) {
-      setError("Erro ao atualizar perfil." + err);
+    } catch (error) {
+      console.error("Erro ao atualizar perfil:", error);
+      form.setError("root", { message: "Erro ao atualizar o perfil." });
     } finally {
       setLoading(false);
     }
   };
 
+  if (!session) return;
+
   return (
     <section className="flex min-h-screen bg-zinc-50 px-4 dark:bg-transparent py-5">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-muted m-auto h-fit w-full max-w-[600px] overflow-hidden rounded-lg border shadow-md"
-      >
-        <div className="bg-card p-8 pb-6">
-          <div className="flex justify-center relative bottom-4">
-            <Image src="/logo.png" height={40} width={180} alt="DPVAT Paraná" />
-          </div>
-          <h1 className="text-center text-xl font-semibold">
-            Complete seu Cadastro
-          </h1>
-          <p className="text-center text-sm mb-6">
-            Bem-vindo, {session.user?.name}!
-          </p>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="bg-muted m-auto h-fit w-full max-w-[600px] overflow-hidden rounded-lg border shadow-md"
+        >
+          <div className="bg-card p-8 pb-6">
+            <div className="flex justify-center relative bottom-4">
+              <Image
+                src="/logo.png"
+                height={40}
+                width={180}
+                alt="DPVAT Paraná"
+              />
+            </div>
+            <h1 className="text-center text-xl font-semibold">
+              Complete seu Cadastro
+            </h1>
+            <p className="text-center text-sm mb-6">
+              Bem-vindo, {session.user?.name}!
+            </p>
 
-          <div className="grid grid-cols-2 gap-4">
-            <div className="space-y-2">
-              <Label htmlFor="cpf">CPF </Label>
-              <Label className="text-gray-500 text-[12px]">
-                Apenas numeros
-              </Label>
-              <Input
-                type="text"
+            <div className="grid grid-cols-2 gap-4">
+              <FormField
+                control={form.control}
                 name="cpf"
-                value={formData.cpf}
-                onChange={handleChange}
-                required
-                maxLength={14}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>CPF</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu CPF" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="data_nasc">Data de Nascimento</Label>
 
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button variant="outline" className="w-full">
-                    {formData.data_nasc
-                      ? format(new Date(formData.data_nasc), "dd/MM/yyyy")
-                      : "Selecione a data"}
-                  </Button>
-                </PopoverTrigger>
+              <FormField
+                control={form.control}
+                name="data_nasc"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Data de Nascimento</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button variant="outline" className="w-full">
+                          {field.value
+                            ? format(new Date(field.value), "dd/MM/yyyy")
+                            : "Selecione a data"}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-full mt-2">
+                        <Calendar
+                          selected={
+                            field.value ? new Date(field.value) : undefined
+                          }
+                          onDayClick={(date: Date) => {
+                            const formattedDate = format(date, "yyyy-MM-dd");
+                            field.onChange(formattedDate);
+                          }}
+                        />
+                      </PopoverContent>
+                    </Popover>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                <PopoverContent className="w-full mt-2">
-                  <Calendar
-                    selected={
-                      formData.data_nasc
-                        ? new Date(formData.data_nasc)
-                        : undefined
-                    }
-                    onDayClick={(date: Date) => {
-                      const formattedDate = format(date, "yyyy-MM-dd");
-                      setFormData({ ...formData, data_nasc: formattedDate });
-                    }}
-                  />
-                </PopoverContent>
-              </Popover>
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="cep">CEP </Label>
-              <Label className="text-gray-500 text-[12px]">
-                Apenas numeros
-              </Label>
-              <Input
-                type="text"
+              <FormField
+                control={form.control}
                 name="cep"
-                value={formData.cep}
-                onChange={handleChange}
-                required
-                maxLength={10}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>CEP</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu CEP" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
-            </div>
 
-            <div className="space-y-2">
-              <Label htmlFor="rua">Rua</Label>
-              <Input
-                type="text"
+              <FormField
+                control={form.control}
                 name="rua"
-                value={formData.rua}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  const inputValue = e.target.value;
-                  if (inputValue.startsWith("Rua")) {
-                    setFormData({ ...formData, rua: inputValue });
-                  } else {
-                    setFormData({ ...formData, rua: "Rua" });
-                  }
-                }}
-                required
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Rua</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Digite sua rua"
+                        {...field}
+                        onChange={(e) => {
+                          const value = e.target.value;
+                          if (value.startsWith("Rua")) {
+                            field.onChange(value);
+                          } else {
+                            field.onChange("Rua");
+                          }
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
-            </div>
 
-            <div className="space-y-2">
-              <Label htmlFor="bairro">Bairro</Label>
-              <Input
-                type="text"
+              <FormField
+                control={form.control}
                 name="bairro"
-                value={formData.bairro}
-                onChange={handleChange}
-                required
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Bairro</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu bairro" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
-            </div>
 
-            <div className="space-y-2">
-              <Label htmlFor="numero">Número</Label>
-              <Input
-                type="text"
+              <FormField
+                control={form.control}
                 name="numero"
-                value={formData.numero}
-                onChange={handleChange}
-                required
-                maxLength={6}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Número</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite o número" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
             </div>
+
+            <Button type="submit" className="mt-6 w-full" disabled={loading}>
+              {loading ? (
+                <Loader2Icon className="animate-spin mr-2" />
+              ) : (
+                "Salvar"
+              )}
+            </Button>
           </div>
-
-          <Button type="submit" className="mt-6 w-full" disabled={loading}>
-            {loading ? "Salvando..." : "Salvar"}
-          </Button>
-
-          {error && (
-            <p className="mt-2 text-center text-sm text-red-500">{error}</p>
-          )}
-        </div>
-      </form>
+        </form>
+      </Form>
     </section>
   );
 }
