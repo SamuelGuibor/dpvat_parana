b67919a797c1685a4edc7c4a409c6408171ceed3
SamuelGuibor <guiborsamuel@gmail.com>
2025-03-25 15:47:39 -0300

    porra

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,7 +0,0 @@
-import NextAuth from "next-auth"
-import { authOptions } from "@/app/_lib/auth"
-//import { EmailProvider } from "next-auth/providers/email"
-
-const handler = NextAuth(authOptions)
-
-export { handler as GET, handler as POST }
