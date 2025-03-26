25358c08d008fbec3501d44bca9789f3ba6146a7
Otto <ottoengelhardt18@gmail.com>
2025-03-26 20:24:43 -0300

    feat: add blog page

---
+++ app/blog/header.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center w-full max-w-7xl mx-auto py-4 px-6">
+      {/* Logo */}
+      <div>
+        <Image
+          src="/logo.png"
+          alt="Logo Paraná Seguros"
+          width={120}
+          height={100}
+        />
+      </div>
+
+      {/* Ícones */}
+      <div className="flex gap-4">
+        <Link
+          href="https://www.facebook.com/paranadpvat/"
+          aria-label="Veja nosso Facebook"
+        >
+          <FaFacebook size={30} className="text-black hover:text-blue-600" />
+        </Link>
+        <Link
+          href="https://www.instagram.com/paranadpvat/"
+          aria-label="Veja nosso Instagram"
+        >
+          <FaInstagram size={30} className="text-black hover:text-pink-500" />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
