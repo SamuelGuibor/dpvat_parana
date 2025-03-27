fe22ea1c6faa21fa753a49594bc665b9e10cd912
Otto <ottoengelhardt18@gmail.com>
2025-03-27 14:41:37 -0300

    feat: add progress bar to status page

--- app/status/page.tsx
+++ app/status/page.tsx
@@ -1,11 +1,18 @@
 import Sidebar from "../area-do-cliente/section/sidebar";
+import ProgressIndicator from "./status-progress";
 
 const StatusPage = () => {
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row h-screen">
       <Sidebar />
-      <div className="m-8 mt-32 w-full space-y-8 overflow-auto rounded-lg bg-gray-100 p-8 md:mt-8 ">
-        <h1>aaa</h1>
+      <div className="m-8 mt-32 w-full  overflow-auto rounded-lg bg-gray-100 p-8 md:mt-8 ">
+        <h2 className="text-2xl font-semibold">Status</h2>
+        <p className="text-sm font-semibold text-slate-500">
+          Verifique o status do seu processo.
+        </p>
+        <div className="mx-auto pt-[150px]">
+          <ProgressIndicator />
+        </div>
       </div>
     </div>
   );
