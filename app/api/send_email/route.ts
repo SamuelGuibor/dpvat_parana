1ff0f41906a112c5ca41b2daec6a4d4825dbdc74
SamuelGuibor <guiborsamuel@gmail.com>
2025-12-16 13:58:22 -0300

    dale

---
+++ app/api/send_email/route.ts
@@ -0,0 +1,18 @@
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const { data } = await resend.emails.send({
+      from: 'mucaguibor@gmail.com',
+      to: 'mucaguibor@gmail.com',
+      subject: 'Olá Mundo',
+      html: '<strong>Funcionou!</strong>',
+    });
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error });
+  }
+}
--- app/send_email/page.tsx
+++ app/send_email/page.tsx
@@ -3,9 +3,22 @@
 import { AppSidebar } from "@/app/_components/app-sidebar";
 import { SiteHeader } from "@/app/_components/site-header";
 import { SidebarInset, SidebarProvider } from "@/app/_components/ui/sidebar";
+import { Button } from "../_components/ui/button";
 
 
 export default function Send_Email() {
+
+  async function mandarEmail() {
+    try {
+      await fetch("/api/send_email", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      });
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
   
 
   return (
@@ -16,6 +29,7 @@ export default function Send_Email() {
         <div className="flex flex-1 flex-col w-full">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                <Button onClick={mandarEmail}>Mandar Email</Button>
             </div>
           </div>
         </div>
