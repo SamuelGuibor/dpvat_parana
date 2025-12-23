68f42817c40bb2f6d669bcf2af9b18b30d1008ff
SamuelGuibor <guiborsamuel@gmail.com>
2025-12-23 18:11:57 -0300

    dale

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,29 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        <Script id="mixpanel-init" strategy="afterInteractive">
+          {`(function(e,c){if(!c.__SV){var l,h;window.mixpanel=c;c._i=[];c.init=function(q,r,f){
+            function t(d,a){var g=a.split(".");2==g.length&&(d=d[g[0]],a=g[1]);
+            d[a]=function(){d.push([a].concat(Array.prototype.slice.call(arguments,0)))}}
+            var b=c;"undefined"!==typeof f?b=c[f]=[]:f="mixpanel";b.people=b.people||[];
+            b.toString=function(d){var a="mixpanel";"mixpanel"!==f&&(a+="."+f);
+            d||(a+=" (stub)");return a};b.people.toString=function(){
+            return b.toString(1)+".people (stub)"};
+            l="disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking has_opted_out_tracking clear_opt_in_out_tracking start_batch_senders start_session_recording stop_session_recording people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user people.remove".split(" ");
+            for(h=0;h<l.length;h++)t(b,l[h]);
+            c._i.push([q,r,f])};c.__SV=1.2;
+            var k=e.createElement("script");k.type="text/javascript";k.async=!0;
+            k.src="https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";
+            e=e.getElementsByTagName("script")[0];e.parentNode.insertBefore(k,e)}
+          })(document,window.mixpanel||[]);
+
+          mixpanel.init('3a16647810beddeb9b99612b7af13120', {
+            autocapture: true,
+            record_sessions_percent: 100,
+          });
+          `}
+        </Script>
+
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-4ETETM1CP7"
@@ -99,7 +122,7 @@ export default function RootLayout({
       <body className={`${mulish.className} antialiased`}>
         <AuthProvider>{children}</AuthProvider>
         <Toaster />
-        <script type="text/javascript" async 
+        <script type="text/javascript" async
           src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/549d8d30-be36-4f81-860f-9377b7717532-loader.js" >
         </script>
         <script
