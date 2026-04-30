27ee79320ce11a3e7672cb9daaa1db29bac017a7
Samuel Guibor <guiborsamuel@gmail.com>
2026-04-30 16:42:02 -0300

    adicionando trello

--- app/api/notification/route.ts
+++ app/api/notification/route.ts
@@ -1,15 +1,5 @@
-// import { NextResponse } from 'next/server';
-// import { db } from '@/app/_lib/prisma';
+import { NextResponse } from 'next/server';
 
 export async function GET() {
-  // const notifications = await db.notification.findMany({
-  //   orderBy: { createdAt: 'desc' },
-  // });
-
-  // return NextResponse.json(notifications, {
-  //   headers: {
-  //     'Cache-Control': 'no-store',
-  //   },
-  // });
-  console.log("boa!")
+  return NextResponse.json({ message: 'ok' });
 }
