7c405dbf6fc923e8a9fbbd9b16c8f2510a5498aa
Samuel Guibor <guiborsamuel@gmail.com>
2026-05-11 12:15:33 -0300

    dale

--- app/_types/next-auth.d.ts
+++ app/_types/next-auth.d.ts
@@ -1,5 +1,10 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
 import NextAuth from "next-auth";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
 import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
