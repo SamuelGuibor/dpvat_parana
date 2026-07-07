8ab9c539ffab32ee2f67d4d904e0715c24b333b7
Samuel Guibor <guiborsamuel@gmail.com>
2026-07-07 15:17:42 -0300

    espaço de trabalho

--- app/nova-dash/workspace/chat/NewChannelDialog.tsx
+++ app/nova-dash/workspace/chat/NewChannelDialog.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 
 import { useMemo, useState } from 'react';
