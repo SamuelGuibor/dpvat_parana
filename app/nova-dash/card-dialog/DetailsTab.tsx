0e49db6cdf74643f09475f3e137b27d7ca0730bb
Samuel Guibor <guiborsamuel@gmail.com>
2026-07-12 18:52:26 -0300

    Algumas coisas

--- app/nova-dash/card-dialog/DetailsTab.tsx
+++ app/nova-dash/card-dialog/DetailsTab.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-unused-vars */
 import type { ReactNode } from 'react';
